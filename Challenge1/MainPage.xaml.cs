a6994de2259f1d55a92bdc8f73fabc4e8a5b28ca
JacobBradshaw-WilsonCTS <jacob-b-w@outlook.com>
2018-06-13 09:31:26 +0100

    Initial check-in of module Challenge1

---
+++ Challenge1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Challenge1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Added(object sender, EventArgs e)
+        {
+            DisplayAlert("Added", "Song has been added", "OK");
+        }
+    }
+}
